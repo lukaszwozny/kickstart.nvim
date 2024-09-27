65d5a65034934f64f3a59bbead9641ca6b04702d
Lukasz Wozny <kontakt@lukaszwozny.com>
2024-09-27 20:46:51 +0200

    fsadffffafsad

--- init.lua
+++ init.lua
@@ -526,6 +526,8 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        tailwindcss = {},
+        html = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
---
+++ lua/custom/plugins/auto-pairs.lua
@@ -0,0 +1,5 @@
+-- Primegen Harpoon 2
+return {
+  'jiangmiao/auto-pairs',
+  config = function() end,
+}
